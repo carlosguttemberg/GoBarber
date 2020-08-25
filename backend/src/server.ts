795b4e11b1a6ca2c8518a685a6db5edf1df01448
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-08-24 21:55:47 -0300

    Iniciando a aplicacao GoBarber

---
+++ backend/src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.post('/users', (request, response) => {
+    const { name, email } = request.body;
+
+    const user = {
+        name,
+        email,
+    };
+
+    return response.json({ message: 'hello world' });
+});
+
+app.listen(3333, () => {
+    console.log('Server started on port 3333');
+});

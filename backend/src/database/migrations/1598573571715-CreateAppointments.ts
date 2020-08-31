328f8dc9ebc088165821171218c7f87dd033505a
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-08-31 15:49:13 -0300

    Criando persistencia com o banco de dados

--- backend/src/database/migrations/1598573571715-CreateAppointments.ts
+++ backend/src/database/migrations/1598573571715-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1598573571715
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',

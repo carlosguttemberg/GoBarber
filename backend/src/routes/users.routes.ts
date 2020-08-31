6fce8587fc25d7c61b83cb7fdfaf6ccc8b72ff8e
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-08-31 18:31:41 -0300

    Criando autenticação de usuario

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
 
-const users = Router();
+const usersRouter = Router();
 
-users.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response) => {
     try {
         const { name, email, password } = request.body;
 
@@ -23,4 +23,4 @@ users.post('/', async (request, response) => {
     }
 });
 
-export default users;
+export default usersRouter;

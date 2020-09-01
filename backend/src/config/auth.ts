fe995c0bc6e9c21a5d0539da84fc45ac6c418592
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-09-01 12:09:37 -0300

    Criando o middleware de autenticacao

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '7f558d6f9c4235f77daf63f3dea424f5',
+        expiresIn: '1d',
+    },
+};
--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -32,9 +33,11 @@ class AuthenticateUserServer {
             throw new Error('Incorrect email/password combination.');
         }
 
-        const token = sign({}, '7f558d6f9c4235f77daf63f3dea424f5', {
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({}, secret, {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn,
         });
 
         return { user, token };

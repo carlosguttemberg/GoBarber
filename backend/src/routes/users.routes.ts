77ee68ff652a34cbd787862d172979e547407d41
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-09-01 13:11:17 -0300

    Criando upload e exibicao de arquivos

--- backend/src/routes/users.routes.ts
+++ backend/src/routes/users.routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
     try {
@@ -18,9 +25,31 @@ usersRouter.post('/', async (request, response) => {
         delete user.password;
 
         return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
+    } catch (error) {
+        return response.status(400).json({ error: error.message });
     }
 });
+
+usersRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        try {
+            const updateUserAvatar = new UpdateUserAvatarService();
+
+            const user = await updateUserAvatar.execute({
+                user_id: request.user.id,
+                avatarFilename: request.file.filename,
+            });
+
+            delete user.password;
+
+            return response.json(user);
+        } catch (error) {
+            return response.status(400).json({ error: error.message });
+        }
+    },
+);
 
 export default usersRouter;

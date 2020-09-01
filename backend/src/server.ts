a484c1b8dd3046baceb6408c48aa842836c50ecc
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-09-01 13:42:55 -0300

    Criando tratativa global de erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,34 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
-
 app.use('/files', express.static(uploadConfig.directory));
-
 app.use(routes);
 
-app.post('/users', (request, response) => {
-    const { name, email } = request.body;
+app.use(
+    (error: Error, request: Request, response: Response, _: NextFunction) => {
+        if (error instanceof AppError) {
+            response.status(error.statusCode).json({
+                status: 'error',
+                message: error.message,
+            });
+        }
 
-    const user = {
-        name,
-        email,
-    };
-
-    return response.json({ message: 'hello world' });
-});
+        return response.status(500).json({
+            status: 'error',
+            message: 'Internal server error',
+        });
+    },
+);
 
 app.listen(3333, () => {
     console.log('Server started on port 3333');

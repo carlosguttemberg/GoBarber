a13e599c9c7777b39d98cd822171dec9cff15f08
Carlos Guttemberg <guttembergcarlos@gmail.com>
2020-09-22 21:04:40 -0300

    Aplicando inversao de dependencias na geracao de hash de senha e na comparacao de senhas

--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -1,4 +1,3 @@
-import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
@@ -7,6 +6,7 @@ import { inject, injectable } from 'tsyringe';
 import User from '@modules/users/infra/typeorm/entities/User';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 
 interface IRequest {
     email: string;
@@ -21,7 +21,11 @@ interface IResponse {
 @injectable()
 class AuthenticateUserServer {
     constructor(
-        @inject('UsersRepository') private usersRepository: IUsersRepository,
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('HashProvider')
+        private hashProvider: IHashProvider,
     ) {}
 
     public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -31,7 +35,10 @@ class AuthenticateUserServer {
             throw new AppError('Incorrect email/password combination.', 401);
         }
 
-        const passwordMatched = await compare(password, user.password);
+        const passwordMatched = await this.hashProvider.compareHash(
+            password,
+            user.password,
+        );
 
         if (!passwordMatched) {
             throw new AppError('Incorrect email/password combination.', 401);
--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -1,12 +1,17 @@
 import AppError from '@shared/errors/AppError';
 
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 
 describe('CreateUser', () => {
     it('should be able to create a new user', async () => {
         const fakeUsersRepository = new FakeUsersRepository();
-        const createUserService = new CreateUserService(fakeUsersRepository);
+        const fakeHashProvider = new FakeHashProvider();
+        const createUserService = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
 
         const user = await createUserService.execute({
             name: 'John Doe',
@@ -19,7 +24,12 @@ describe('CreateUser', () => {
 
     it('should not be able to create a new user with same email from another', async () => {
         const fakeUsersRepository = new FakeUsersRepository();
-        const createUserService = new CreateUserService(fakeUsersRepository);
+        const fakeHashProvider = new FakeHashProvider();
+
+        const createUserService = new CreateUserService(
+            fakeUsersRepository,
+            fakeHashProvider,
+        );
 
         await createUserService.execute({
             name: 'John Doe',

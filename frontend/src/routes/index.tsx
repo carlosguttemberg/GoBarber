ec46cb3892145afd4d9600cc970cd59c8e0b826f
Carlos Guttemberg <guttembergcarlos@gmail.com>
2021-01-23 13:42:58 -0300

    Criando a parte de recuperacao de senha

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,12 +5,14 @@ import Route from './Route';
 
 import SignIn from '../pages/SingIn';
 import SignUp from '../pages/SingUp';
+import ForgotPassword from '../pages/ForgotPassword';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" exact component={SignUp} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
 
     <Route path="/dashboard" exact component={Dashboard} isPrivate />
   </Switch>
